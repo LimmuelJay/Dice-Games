110dbe8c897d85dabd4f1c20b4bfd43acdcfb0dc
limmuel <limmueljayvaldez@gmail.com>
2024-01-16 16:41:41 +0800

    changed location

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 console.log('test')
 const randomNumber1 = Math.ceil(Math.random() * 6);
 const randomNumber2 = Math.ceil(Math.random() * 6);
-document.querySelector(".img1").setAttribute("src", `./images/dice${randomNumber1}.png`)
-document.querySelector(".img2").setAttribute("src", `./images/dice${randomNumber2}.png`)
+document.querySelector(".img1").setAttribute("src", `./dice${randomNumber1}.png`)
+document.querySelector(".img2").setAttribute("src", `./dice${randomNumber2}.png`)
 if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
 } else if (randomNumber2 > randomNumber1) {
